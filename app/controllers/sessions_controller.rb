bc2850c78196ca6a70741c010d9775fc162e945c
ihcugamay <ymgc1994@icloud.com>
2023-01-26 23:02:18 +0900

    feat: Add sessions create an destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,29 @@
 
 # SessionsController
 class SessionsController < ApplicationController
-  def new
+  def new; end
 
+  def create
+    user = User.find_by(name: session_params[:name])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There was something wrong with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:name, :password)
   end
 end
