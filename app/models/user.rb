d17ced70e6e425878ff9654a5a5fb6d11ecd25e1
ihcugamay <ymgc1994@icloud.com>
2023-01-26 21:28:43 +0900

    feat: Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'test1', password: 'password')
+User.create(name: 'test2', password: 'password')
+User.create(name: 'test3', password: 'password')
+User.create(name: 'test4', password: 'password')
+User.create(name: 'test5', password: 'password')
